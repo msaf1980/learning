a21c593768616813084b5dde2529d7e37944f3ac
Michail Safronov <msaf1980@gmail.com>
2019-04-12 09:10:58 +0500

    add fork for client connection

--- network/hello/hellosrv/src/main.c
+++ network/hello/hellosrv/src/main.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
+#include <unistd.h>
 #include <signal.h>
+#include <sys/socket.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 
 #include <netutils/netutils.h>
@@ -9,6 +12,8 @@
 /* #define BACKLOG 20 */
 #define BACKLOG SOMAXCONN
 
+#define BUFSIZE 4096
+
 int running = 1;
 
 struct config {
@@ -16,13 +21,56 @@ struct config {
   int port;
 };
 
+int server_session(int sess_fd, const char *ip, const u_short port)
+{
+    char buf[BUFSIZE];
+    strcpy(buf, "Hi there!\n");
+    ssize_t len = strlen(buf);
+    for (int i = 0; i < 5; i++) {
+      if (send(sess_fd, buf, len, MSG_NOSIGNAL) == -1) {
+        _LOG_ERROR_ERRNO(root_logger, "send on client connection from %s:%d: %s", 
+                         errno, ip, port);
+        break;
+      }
+    }
+    /* _LOG_INFO(root_logger, "close client connection from %s:%d", ip, port); */
+    close(sess_fd);
+}
+
 int loop_fork(int srv_fd) {
   int ec = 0;
+  SA_IN client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
+  char ipbuf[INET_ADDRSTRLEN];
   while (running) {
+    int sess_fd = accept(srv_fd, (SA *) &client_addr, &client_addr_len);
+		if (sess_fd == -1) {
+			_LOG_ERROR_ERRNO(root_logger, "%s on socket %d: %s", errno, "accept", srv_fd);
+			continue;
+		}
 
+		/* Format client IP address */
+    if (getnameinfo( (SA *) &client_addr, client_addr_len, 
+                     ipbuf, INET_ADDRSTRLEN, 0,0, NI_NUMERICHOST) == 0) {
+      _LOG_INFO(root_logger, "connect from %s:%d", ipbuf, client_addr.sin_port);
+    } else {
+      _LOG_ERROR(root_logger, "invalid address for socket %d", sess_fd);
+    }
 
+		pid_t pid = fork();
+    if (pid == -1) {
+      _LOG_ERROR_ERRNO(root_logger, "fork on client connection from %s:%d: %s", 
+                       errno, ipbuf, client_addr.sin_port);
+    } else if (pid == 0) {
+      /* child process */
+      close(srv_fd);
+      server_session(sess_fd, ipbuf, client_addr.sin_port);
+      exit(0);
+    }
+    close(sess_fd);
   }
 EXIT:
+  close(srv_fd);
   return ec;
 }
 
@@ -32,7 +80,7 @@ int start_server(const struct config *conf) {
 	SA_IN srv_addr;
 
 	if ( (srv_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ) {
-     ec = EXIT_FAILURE; _LOG_ERROR_ERRNO(root_logger, "%s: %s", errno, "socket"); goto EXIT;
+    ec = EXIT_FAILURE; _LOG_ERROR_ERRNO(root_logger, "%s: %s", errno, "socket"); goto EXIT;
   }
 	set_reuseaddr(srv_fd);
 
@@ -65,6 +113,7 @@ EXIT:
 int main(int argc, char * const argv[]) {
     int ec = 0, pid;
     int background = 0;
+    int closefd = FD_NOCLOSE;
     const char *name = "hellosrv";
     struct config conf;
     conf.ip = NULL;
@@ -72,7 +121,10 @@ int main(int argc, char * const argv[]) {
 
     openlog(name, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL0);
 
-    pid = daemon_init(background, 1, background);
+    if (background)
+      closefd = FD_CLOSE_STD;
+
+    pid = daemon_init(background, 1, closefd);
     if (pid ==  0) { /* child process */
       ec = start_server(&conf);
     } else if (pid < 0) {
